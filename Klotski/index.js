d539a50b4a013f429cf40ef0b1c643ae6f8fe891
qibinxian <50369436+qibinxian@users.noreply.github.com>
2019-07-02 15:08:19 +0800

    Add files via upload

---
+++ Klotski/index.js
@@ -0,0 +1,1 @@
+window.screenOrientation="sensor_landscape",loadLib("libs/laya.core.js"),loadLib("libs/laya.webgl.js"),loadLib("libs/laya.ui.js"),loadLib("js/array.js"),loadLib("js/unit.js"),loadLib("js/index.js");
